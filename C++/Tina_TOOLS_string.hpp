015cbbcec39a5764885130ce1c699db23341a9e6
Tina-1300 <tina.xytrfgthuji1348@gmail.com>
2024-07-31 11:00:14 +0200

    version 0.2 modification d'une méthode dans la class Tina_TOOLS_string.hpp

--- C++/Tina_TOOLS_string.hpp
+++ C++/Tina_TOOLS_string.hpp
@@ -136,25 +136,14 @@ public:
         }
     }
 
-
-    static std::string Tina_convert_string_to_binary(std::string str){
-        std::string  binaryString;
-        for (std::size_t i = 0; i < str.size(); i++){
-            //std::cout << std::bitset<8>(str[i]) << std::endl;
-            binaryString += std::bitset<8>(str[i]).to_string();
-        }
-        return  binaryString;
-    }
-
     /**
-     * @fn Tina_is_palindrome()
-     * @param [std::string strr] attend une chaine de caractère a vérifier si il et palindrom
-     * @param [int Tina_return_bool_] et pas obligatoir mais si vous l'utiliser en mettant Tina_return_bool aulieu de retourner le mot entre il returnera Tina_TRUE ou Tina_FALSE
-     * @note Cette fonction et une function surcharger qui permet de voir si un mot et palindrom
+     * @fn Tina_convert_string_to_binary()
+     * @param [std::string str] attend une chaine de caractère a comparer 
+     * @note Cette fonction permet de traduire une chaine de caractère en binaire
      * 
-     * Cette fonction et une function surcharger qui permet de voir si un mot et palindrom
+     * Cette fonction permet de traduire une chaine de caractère en binaire
      * 
-     * Example d'utilisation 1 : 
+     * Example d'utilisation : 
      * 
      * ```c
      * #include <iostream>
@@ -162,27 +151,20 @@ public:
      * 
      * int main(){
      *     Tina_TOOLS_string tina_str;
-     *     std::string p = "txt";
-     *     if(tina_str.Tina_is_palindrome(p, Tina_return_bool) == Tina_TRUE){
-     *         std::cout << "Le mot : " << p << " est bien un palindrom" << std::endl;
-     *     }else if(tina_str.Tina_is_palindrome(p, Tina_return_bool) == Tina_TRUE){
-     *         std::cout << "le mot : " << p << " n'est pas un palindrom" << std::endl;
-     *     }else{
-     *         std::cout << "error" << std::endl;
-     *     }
+     *     std::string p = "de";
+     *     std::cout << tina_str.Tina_convert_string_to_binary(p) std::endl;
      * 
      *     return 0; 
      * }
      * ```
     */
-    static std::string Tina_is_palindrome(std::string strr){
-        std::string p = strr;
-        std::string l = Tina_reverseString(p);
-        if(l == p){
-            return l; 
-        }else{
-            return "is note palindrome";
+    static std::string Tina_convert_string_to_binary(std::string str){
+        std::string  binaryString;
+        for (std::size_t i = 0; i < str.size(); i++){
+            //std::cout << std::bitset<8>(str[i]) << std::endl;
+            binaryString += std::bitset<8>(str[i]).to_string();
         }
+        return  binaryString;
     }
 
     /**
